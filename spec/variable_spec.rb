077e69bc1542474e428c8ac483c4ce242f844fd8
Rashad Goddard <rashadjgoddard@gmail.com>
2020-10-10 23:23:15 +0000

    Automatically backed up by Learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,10 +1,19 @@
 # Code your solution in variable.rb
 describe "./variable.rb" do
+ require_relative '../variable.rb'
 
-  it "defined a local variable called greeting and set it equal to 'Hello World'" do
-    greeting = get_variable_from_file('./variable.rb', "greeting")
-
-    expect(greeting).to eq("Hello World")
-  end
-
+RSpec.configure do |config|
+  config.order = 'default'
 end
+
+def get_variable_from_file(file, variable)
+  file_scope = binding
+  file_scope.eval(File.read(file))
+
+  begin
+    return file_scope.local_variable_get(variable)
+  rescue NameError
+    raise NameError, "local variable `#{variable}` not defined in #{file}."
+  end
+end
+  
